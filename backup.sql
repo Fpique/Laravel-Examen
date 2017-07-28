f1adafcccfb16c1246eef4aec80ff5ff91fd4aae
fpique <florent.piquepaille@gmail.com>
2017-07-28 10:45:22 +0000

    added sql

---
+++ backup.sql
@@ -0,0 +1,77 @@
+-- MySQL dump 10.13  Distrib 5.5.57, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: boutique
+-- ------------------------------------------------------
+-- Server version	5.5.57-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `marques`
+--
+
+DROP TABLE IF EXISTS `marques`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `marques` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_marque` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marques`
+--
+
+LOCK TABLES `marques` WRITE;
+/*!40000 ALTER TABLE `marques` DISABLE KEYS */;
+INSERT INTO `marques` VALUES (1,'Puma'),(2,'Pokemon'),(3,'Ralph Lauren'),(4,'Nike'),(5,'Levis');
+/*!40000 ALTER TABLE `marques` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shirts`
+--
+
+DROP TABLE IF EXISTS `shirts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shirts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_marque` int(11) NOT NULL,
+  `taille` varchar(45) NOT NULL,
+  `image` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shirts`
+--
+
+LOCK TABLES `shirts` WRITE;
+/*!40000 ALTER TABLE `shirts` DISABLE KEYS */;
+INSERT INTO `shirts` VALUES (1,1,'S',''),(2,2,'M',''),(3,3,'L',''),(4,4,'XL','');
+/*!40000 ALTER TABLE `shirts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-07-28 10:43:20
